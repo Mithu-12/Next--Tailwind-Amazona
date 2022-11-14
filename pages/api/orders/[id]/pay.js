3aeb12f964cd316c3c1a2a8631b1db5c139f4c7c
Mithu-12 <mithuvowmick96@gmail.com>
2022-11-14 00:23:58 -0500

    last

--- pages/api/orders/[id]/pay.js
+++ pages/api/orders/[id]/pay.js
@@ -1,12 +1,12 @@
-import { getSession } from 'next-auth/react';
+// import { getSession } from 'next-auth/react';
 import Order from '../../../../models/Order';
 import db from '../../../../utils/db';
 
 const handler = async (req, res) => {
-  const session = await getSession({ req });
-  if (!session) {
-    return res.status(401).send('Error: signin required');
-  }
+  // const session = await getSession({ req });
+  // if (!session) {
+  //   return res.status(401).send('Error: signin required');
+  // }
 
   await db.connect();
   const order = await Order.findById(req.query.id);
