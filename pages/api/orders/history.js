872276b62d35a8609b2a4233e4c478a309aa5426
Mithu-12 <mithuvowmick96@gmail.com>
2022-11-14 01:27:40 -0500

    last

--- pages/api/orders/history.js
+++ pages/api/orders/history.js
@@ -2,10 +2,23 @@ import { getSession } from 'next-auth/react';
 import Order from '../../../models/Order';
 import db from '../../../utils/db';
 
-const handler = async (res) => {
-  const session = await getSession();
+// const handler = async (res) => {
+//   const session = await getSession();
+//   if (!session) {
+//     return res.status(401).send({ message: 'signin not required' });
+//   }
+//   const { user } = session;
+//   await db.connect();
+//   const orders = await Order.find({ user: user._id });
+//   await db.disconnect();
+//   res.send(orders);
+// };
+
+// export default handler;
+const handler = async (req, res) => {
+  const session = await getSession({ req });
   if (!session) {
-    return res.status(401).send({ message: 'signin not required' });
+    return res.status(401).send('singin required');
   }
   const { user } = session;
   await db.connect();
@@ -13,5 +26,4 @@ const handler = async (res) => {
   await db.disconnect();
   res.send(orders);
 };
-
 export default handler;
